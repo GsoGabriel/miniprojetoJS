fddb898908f50086234d1a5fb316d21507a6c246
Gabriel Oliveira <gabriel.sorj@gmail.com>
2020-10-30 16:44:05 -0300

    primeiro commit

---
+++ mainscript.js
@@ -0,0 +1,19 @@
+function fileUpload() {
+	alert("File Upload funcionando");
+}
+
+function filterGrayscale() {
+	alert("Filtro Grayscale funcionando");
+}
+
+function filterRainbow() {
+	alert("Filtro rainbow funcionando");
+}
+
+function filterRed() {
+	alert("Filtro red funcionando");
+}
+
+function reset() {
+	alert("Reset funcionando");
+}
